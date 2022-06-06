fc14faa6735563c534b150d7b23dea8d685e01ff
ImtiazHossainShovon <90201587+ImtiazHossainShovon@users.noreply.github.com>
2022-06-06 17:09:15 +0600

    final changes

--- static/js/pdf.js
+++ static/js/pdf.js
@@ -7,7 +7,7 @@ window.onload = function(){
         console.log(ticket);
         console.log(window);
         var opt = {
-            filename : 'bdRailwayTicket.pdf',
+            filename : 'myTrainTicket.pdf',
         };
         html2pdf().from(ticket).set(opt).save();
     })
